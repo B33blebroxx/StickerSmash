4f7769599b25424a5635f3de129aea6186921ecb
B33blebroxx <B33blebroxx@gmail.com>
2024-11-21 09:51:17 -0600

    Remove unused components and files; Added home and info screens, made home have a placeholder image and buttons that open an image selector. Also added several other buttons and a modal with limited functionality currently.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,36 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import IonIcons from "@expo/vector-icons/Ionicons";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+    <Tabs screenOptions={{ 
+      tabBarActiveTintColor: '#ffd33d',
+      headerStyle: {
+        backgroundColor: "#25292e",
+      },
+      headerShadowVisible: false,
+      headerTintColor: "#fff",
+      tabBarStyle: {
+        backgroundColor: "#25292e",
+      },
       }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      <Tabs.Screen name="index" options={{
+        title:"Home",
+        tabBarIcon: ({color, focused }) => (
+          <IonIcons name={focused ? "home-sharp" : "home-outline"} size={24} color={color} />
+        ),
+        headerTitle:"Home",
+        headerLeft:()=><></>,
+        headerTitleAlign:"center",
+      }}/>
+      <Tabs.Screen name="about" options={{
+        title:"About",
+        tabBarIcon: ({color, focused }) => (
+          <IonIcons name={focused ? "information-circle" : "information-circle-outline"} size={24} color={color} />
+        ),
+        headerTitle:"About",
+        headerTitleAlign:"center",
+      }}/>
     </Tabs>
   );
 }
